5dcd352452fcbe4ed10bdebadcd3a41deb81cb7b
ariph007 <ariprayoga007@gmail.com>
2024-11-10 20:47:03 +0700

    feat: add dto converter

---
+++ src/main/java/com/lawencon/inventory/converter/ToUpperCase.java
@@ -0,0 +1,15 @@
+package com.lawencon.inventory.converter;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonSerialize(converter = UpperCaseConverter.class)
+@JsonDeserialize(converter = UpperCaseConverter.class)
+public @interface ToUpperCase {
+
+}
